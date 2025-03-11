1b3103cfeba47a9b7ffc666519c8ffa03496da81
andi faizal <andifaizal@MacBook-Pro-andi.local>
2025-03-11 15:38:41 +0700

    reference

---
+++ reference.php
@@ -0,0 +1,7 @@
+<?php
+    $array1 = array(1,2);
+    $x = &$array1[1];   // Unused reference
+    $array2 = $array1;  // reference now also applies to $array2 !
+    $array2[1]=22;      // (changing [0] will not affect $array1)
+    print_r($array1);
+?>
